52130b45bde75f0f6acf6964f9eb3935594f4add
Haamid Mohammed <h.mohammed6@share.epsb.ca>
2018-09-24 12:13:27 -0600

    room 1 finished

---
+++ Discontinuity.py
@@ -0,0 +1,18 @@
+'''
+Title: Discontinuity
+Author: Haamid Mohammed
+Date Created: 2018/09/21
+'''
+import sys
+import random
+import textwrap
+def R1(): # Room 1, Where you begin
+    print("You wake up and u see a spear next to you, and you hear a voice, it says, 'Complete the sword and beat the boss'.")
+    print("You don't know  what that means, so you look around.")
+    print("You see a forest and an open field in front of you, in the open field you see some creatures that you have never seen before and behind you there is a mountain that is too tall and steep to climb.")
+    print("Now you have to make a choice, go to the forest, go to the open field, or try to climb the mountain.")
+    choice1 = input("Forest, open field, or mountain? ")
+
+def R2(): # Room 2, The forest
+	print("You go to the forest, the trees have many fruit but only some seem to be ripe.")
+	print("You don't know if the fruit is poisonous or not")
